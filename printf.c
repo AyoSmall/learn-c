3d16e8fb018fb1157540f2c03614d298fb51b661
AyoSmall <ayosmall8@gmail.com>
2025-12-08 18:53:03 -0400

    Add printf.c

---
+++ printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+	int num = 54321;
+	printf("\n"); // new line character
+	printf("This is a simple C program.\n");
+	printf("We've printed some text already, so here is a number: %d\n\n", num);
+
+	return 0;
+}
