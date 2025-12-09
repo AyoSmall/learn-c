265f8e6e6c49313c9b31d94a6adf415cb55c396a
AyoSmall <ayosmall8@gmail.com>
2025-12-09 18:02:24 -0400

    Add files

---
+++ fathm_ft.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+	
+	puts("\nThis code converts fathoms to feet");
+
+	int feet, fathoms;
+	fathoms = 2;
+	feet = 6 * fathoms;
+
+	printf("There are %d feet in %d fathoms...\n", feet, fathoms);
+
+	return 0;
+}
