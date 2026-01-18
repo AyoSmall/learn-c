d97c0a1f034f36f34348e6f236c0a2cb8650ef44
AyoSmall <ayosmall8@gmail.com>
2026-01-17 22:21:04 -0400

    learning

---
+++ cin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+std::cout << "Enter three numbers :) \n";
+int x{}; //value-initialise an integer variable x 
+int y{};
+int z{};
+
+std::cin >> x >> y >> z; // predefined variale 'cin' gets value from keyboard and store in variable x
+std::cout << "You entered " << x << " and " << y << " and " << z << "\n";
+return 0;
+}
+
---
+++ multiply-by-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+std::cout << "Please enter a number\n";
+int n1{};
+
+std::cin >> n1;
+
+std::cout << "Double that number is: " << (n1 * 2) << "\n";
+
+return 0;
+}
