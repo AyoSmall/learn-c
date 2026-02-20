73828f7c92f58c3d88a4440e9abed093f2f2e8c4
AyoSmall <ayosmall8@gmail.com>
2026-02-20 18:38:35 -0400

    edit typo

--- cin.cpp
+++ cin.cpp
@@ -7,7 +7,7 @@ int x{}; //value-initialise an integer variable x
 int y{};
 int z{};
 
-std::cin >> x >> y >> z; // predefined variale 'cin' gets value from keyboard and store in variable x
+std::cin >> x >> y >> z; // predefined variable 'cin' gets value from keyboard and store in variable x
 std::cout << "You entered " << x << " and " << y << " and " << z << "\n";
 return 0;
 }
