10d8121080c38520a5bd7ae77f0c7332983832b0
Ayo Small <ayosmall8@gmail.com>
2026-04-05 13:24:22 -0400

    Added collectible key key spawns in a random position exit does not ttrigger unless key is collected

--- grid-array.cpp
+++ grid-array.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 // This code implements a navigable grid using C-style arrays
 int main(){
 
     constexpr int y{3},x{5}; // y{rows},x{columns}
-    std::string grid[y][x] = {};
+    // std::string grid[y][x] = {};
     int player_y{0}, player_x{0};
     int exit_y{y-1},exit_x{x-1};
+
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    int key_y{std::rand() % y};
+    int key_x(std::rand() % x);
+    bool has_key{false};
     std::string input;
 
     // Sprites
     std::string grid_img = "| |";
     std::string player_img = "|o|";
     std::string exit_img = "|0|";
+    std::string key_img = "|#|";
+    std::string wall_img = "|||";
+
     
     std::cout << "Would you like to play? y/n   ";
     std::cin >> input;
@@ -27,7 +37,8 @@ int main(){
     
     // Render grid as text
     while (true){
-        std::cout << "position (x,y): (" << player_x << "," << player_y << ")\n";
+        std::cout << "player (x,y): (" << player_x << "," << player_y << ")\n";
+        std::cout << "key (x,y): (" << key_x << "," << key_y << ")\n";
         for (int i = 0; i < y; ++i){
             for (int j = 0; j < x; ++j){
                 if (i == player_y && j == player_x){
@@ -35,6 +46,9 @@ int main(){
                 }
                 else if (i == exit_y && j == exit_x){
                     std::cout << exit_img;
+                }
+                else if (i == key_y && j == key_x && !has_key){
+                    std::cout << key_img;
                 }
                 else std::cout << grid_img;
             }
@@ -51,18 +65,18 @@ int main(){
         if (input == "a" && player_x > 0) --player_x;
         if (input == "d" && player_x < (x-1)) ++player_x;
 
-        if (player_x == exit_x && player_y == exit_y){
+        if (player_x == key_x && player_y == key_y) has_key = true;
+        if (player_x == exit_x && player_y == exit_y && has_key){
             std::cout << "You Escaped !!!\n\n";
             break;
         }
 
         std::system("clear"); // Linux terminal
-        system("cls"); // Windows terminal
+        std::system("cls"); // Windows terminal
     }
 
     return 0;
 }
-
 
 /*
 // todo:
