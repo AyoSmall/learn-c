d75015480eeb0902a2694254457d2e0f993c2c02
Ayo Small <ayosmall8@gmail.com>
2026-04-05 20:54:12 -0400

    Added functions.cpp Practice understanding of functions Core guidelines to avoid errors

---
+++ functions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+
+
+    return 0;    
+}
+
+/*
+// Function definition: 
+// Function call: 
+// 
+// Where can function be defined?
+// - internal
+// - internal 
+// - external 
+// 
+// Function argument scope?
+// - 
+*/
