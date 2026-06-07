7f6cd4eed85c96c53276b38f503768f0ef22745a
Ayo <ayosmall8@gmail.com>
2026-06-06 22:28:33 -0400

    2D array example

--- array-2d.c
+++ array-2d.c
@@ -4,8 +4,31 @@
 
 int main()
 {
-    int map[10][10];    // [layer][row][column]
+    int map[10][10] = {0};    // [layer][row][column] [z][y][x] [k][j][i]
+    int pos = 0;
 
+    // populating a 2d array
+    for (int i = 0; i < 10; ++i)
+    {
+        for (int j = 0; j < 10; ++j)
+        {
+            map[j][i] = pos;
+            pos += 1;
+        }
+    }
+
+    // printing a 2d array
+    printf("Printing a 2d array:\n\n");
+
+    for (int i = 0; i < 10; ++i)
+    {
+        for (int j = 0; j < 10; ++j)
+        {
+            if (map[j][i] < 10) printf("0");
+            printf("%d ", map[j][i]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
