e772842b67a23cdde55260734eff9ac50db55e73
Ayo <ayosmall8@gmail.com>
2026-08-04 09:03:50 -0400

    worked on pop_back() Next time, convert to custom array struct

--- data-structures/array.c
+++ data-structures/array.c
@@ -6,22 +6,29 @@
     - index + (i * element_size)
     This code manipulates data in an array
     The plan is to replace the standard C language array with a custom implementation.
+
+    TODO:
+        - create custom array struct
+        - edit functions to work with struct
+        - edit main to test array
+        - convert array struct/functions to dynamic array
+        - finishing touches for portability as custom integer array type
 */
 
 
 #include <stdio.h>
 #define CAPACITY 10
 
-void array_init(int* head, int capacity, int to_push);
 void array_print(int* head, int capacity);
-int array_push(int* head, int* size, int capacity, int to_push);
-int array_push_many(int* head, int* size, int capacity, int to_push, int to_push_amount);
+void array_init(int* head, int capacity, int to_push);
+int array_push_back(int* head, int* size, int capacity, int to_push);
+int array_push_back_many(int* head, int* size, int capacity, int to_push, int to_push_amount);
 
 int main(void)
 {
-    int array[CAPACITY + 1]; // CAPACITY elements and '\0'
+    int array[CAPACITY];
     int array_size = 0;
-    int array_push(int* head, int* size, int capacity, int to_push);
+    // int array_push_back(int* head, int* size, int capacity, int to_push);
 
     // Initialise array contents
     array_init(array, CAPACITY, 0);
@@ -30,13 +37,13 @@ int main(void)
     array_print(array, CAPACITY);
 
     // Push values to array
-    array_push(array, &array_size, CAPACITY, 69);
+    array_push_back(array, &array_size, CAPACITY, 69);
 
     // Print array contents
     array_print(array, CAPACITY);
 
     // Push multiple values to array
-    array_push_many(array, &array_size, CAPACITY, 8, 9);
+    array_push_back_many(array, &array_size, CAPACITY, 8, 9);
 
     // Print array contents
     array_print(array, CAPACITY);
@@ -45,6 +52,15 @@ int main(void)
 }
 
 
+
+void array_print(int* head, int size)
+{
+    printf("\n\n");
+    for (int i = 0; i < size; ++i)
+    {
+        printf("%d ", *(head + i));
+    }
+}
 
 void array_init(int* head, int capacity, int to_push)
 {
@@ -55,16 +71,7 @@ void array_init(int* head, int capacity, int to_push)
     }
 }
 
-void array_print(int* head, int capacity)
-{
-    printf("\n\n");
-    for (int i = 0; i < capacity; ++i)
-    {
-        printf("%d ", *(head + i));
-    }
-}
-
-int array_push(int* head, int* size, int capacity, int to_push)
+int array_push_back(int* head, int* size, int capacity, int to_push)
 {
 
     if (*size >= capacity)
@@ -79,7 +86,19 @@ int array_push(int* head, int* size, int capacity, int to_push)
     return 1;
 }
 
-int array_push_many(int* head, int* size, int capacity, int to_push, int to_push_amount)
+// unused arguments to be struct members (remove if still unused)
+int array_pop_back(int *head, int* size, int capacity)
+{
+    if (*size <= 0)
+    {
+        return 0;
+    }
+
+    *size -= 1; // removes popped from valid memory, but it still exists until overwritten
+    return 1;
+}
+
+int array_push_back_many(int* head, int* size, int capacity, int to_push, int to_push_amount)
 {
     int push_count = 0;
 
@@ -93,7 +112,7 @@ int array_push_many(int* head, int* size, int capacity, int to_push, int to_push
     {
         // printf("\n\nEnter an integer: ");
         // scanf("%d", &to_push);
-        array_push(head, size, capacity, to_push);
+        array_push_back(head, size, capacity, to_push);
         push_count += 1;
     }
 
