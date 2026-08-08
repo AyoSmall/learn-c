8e339f5c62a417040a19435ad2f4de9c15ac1594
Ayo <ayosmall8@gmail.com>
2026-08-07 23:24:06 -0400

    Separated array functions and definition from test file Read TODO

--- data-structures/array-test.c
+++ data-structures/array-test.c
@@ -20,16 +20,10 @@
 #include <array.c>
 #define CAPACITY 10
 
-void array_print(int* head, int capacity);
-void array_init(int* head, int capacity, int to_push);
-int array_push_back(int* head, int* size, int capacity, int to_push);
-int array_push_back_many(int* head, int* size, int capacity, int to_push, int to_push_amount);
-
 int main(void)
 {
-    int array[CAPACITY];
-    int array_size = 0;
-    // int array_push_back(int* head, int* size, int capacity, int to_push);
+    // int array[CAPACITY];
+    // int array_size = 0;
 
     // Initialise array contents
     array_init(array, CAPACITY, 0);
@@ -50,72 +44,4 @@ int main(void)
     array_print(array, CAPACITY);
 
     return 0;
-}
-
-
-
-void array_print(int* head, int size)
-{
-    printf("\n\n");
-    for (int i = 0; i < size; ++i)
-    {
-        printf("%d ", *(head + i));
-    }
-}
-
-void array_init(int* head, int capacity, int to_push)
-{
-    for (int i = 0; i < capacity; ++i)
-    {
-        *(head + i) = to_push; // access value at head+size and replace
-
-    }
-}
-
-int array_push_back(int* head, int* size, int capacity, int to_push)
-{
-
-    if (*size >= capacity)
-    {
-        return 0;
-    }
-
-    *(head + *size) = to_push; // access value at head+size and replace
-    *size += 1;               // value at address "size" is incremented
-
-
-    return 1;
-}
-
-// unused arguments to be struct members (remove if still unused)
-int array_pop_back(int *head, int* size, int capacity)
-{
-    if (*size <= 0)
-    {
-        return 0;
-    }
-
-    *size -= 1; // removes popped from valid memory, but it still exists until overwritten
-    return 1;
-}
-
-int array_push_back_many(int* head, int* size, int capacity, int to_push, int to_push_amount)
-{
-    int push_count = 0;
-
-    if (capacity - *size < to_push_amount)
-    {
-        printf("\n\nPushed elements exceed available space");
-        return 0;
-    }
-
-    while (push_count < to_push_amount)
-    {
-        // printf("\n\nEnter an integer: ");
-        // scanf("%d", &to_push);
-        array_push_back(head, size, capacity, to_push);
-        push_count += 1;
-    }
-
-    return 1;
 }
